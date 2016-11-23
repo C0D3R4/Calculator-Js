7f0d6806cb65d9ecab5c131793dba11a6d5bd950
C0D3R4 <agl.c0d3r4@gmail.com>
2016-11-23 16:52:19 +0000

    First Commit

---
+++ functions.js
@@ -0,0 +1,50 @@
+var mybtn = document.getElementsByClassName("cbutton");
+var myOutput = document.getElementById("output");
+var myCal = "";
+var myCom = false;
+var mySwitch = false;
+var myOpe = ["+","-","*","/"]; // Look at my Operators
+
+console.log(mybtn);
+  for(var i = 0;i<mybtn.length;i++){
+    mybtn[i].addEventListener("click",function(){
+      var myValue = this.innerHTML;
+        if (myCom || myCal=="0"){
+          myCom = false;
+          myCal = "";
+        }
+        if (myValue == "+"||myValue == "-"||myValue == "*"||myValue == "/") {
+          if(mySwitch){
+            mySwitch = false;
+            if(myOpe.indexOf(myOutput.innerHTML.slice(-1))>-1){
+              myCal=myCal.substring(0,myCal.length -1);
+            } else {
+              myCal = eval(myCal);
+            }
+          }
+         // mySwitch = true;
+        }
+      if (myValue =="=") {
+        myValue = "";
+      if(myOpe.indexOf(myOutput.innerHTML.slice(-1))==-1){
+        myCal = eval(myCal);
+        }
+      }else if (myValue == "C") {
+        myCal = 0;
+        myCom = true;
+      } else if (myOutput.innerHTML.indexOf(".")>-1 && myValue == ".") {
+          myValue = "";
+      }else{
+        myCal=myCal+myValue;
+      }
+      if(mySwitch){
+        mySwitch = false;
+        myCal = eval(myCal);
+      }
+      if (myValue=="+"||myValue=="-"||myValue=="*"||myValue=="/"){
+        mySwitch = true;
+      }
+      myOutput.innerHTML = myCal;
+      console.log('clicked');
+    });
+}
